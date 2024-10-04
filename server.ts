6bb62531dd0c7ec4229d74362f59f2c45e1403d5
Edgar <edgargarteong@gmail.com>
2024-10-04 18:28:01 +0800

    Revert "Setup"
    This reverts commit 669bdfe796972b760d7d5cdce666dc1c6102fc97.

--- server.ts
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config()
-import express, { Request, Response } from 'express';
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {`App started! Listening on Port ${port}`});
